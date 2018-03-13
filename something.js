af54185ec204dc402ad3a0f05e2469ece69be82e
keanedawg <cameronfife@hotmail.com>
2018-03-13 16:24:14 -0600

    created simple calculateRate function

--- something.js
+++ something.js
@@ -10,3 +10,19 @@ app.get('/', function(req, res){
     res.render('index.ejs',{user: "Great User",title:"homepage"});
 });
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+function calculateRate(weight, type) {
+    if (type == "stamped") {
+        return weight * .5;
+    }
+    else if (type == "metered") {
+        return weight * 1.5;
+    }
+    else if (type == "flats") {
+        return weight * 2.5;
+    }
+    else {
+        return weight * 7;
+    }
+}
