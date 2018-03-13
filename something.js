82a45a4b10575ddcc4d8d75e0d6b78086022ce7f
keanedawg <cameronfife@hotmail.com>
2018-03-13 15:55:22 -0600

    set up serving static html files

--- something.js
+++ something.js
@@ -1,5 +1,9 @@
 // I don't even know
 const yabba_dabba_doo = require('express');
 const express = yabba_dabba_doo;
+const app = express();
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
