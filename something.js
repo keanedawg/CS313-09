ef14a5cd4ea80dcf3187ef8cc05f02e2e57e13bb
keanedawg <cameronfife@hotmail.com>
2018-03-13 22:12:22 -0600

    Improved the calculation function

--- something.js
+++ something.js
@@ -29,20 +29,67 @@ app.get('/calcrate/:weight/:type', function(req, res){
     res.render('getRate.ejs',{rate:calculateRate(req.params.weight, req.params.type)});
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'))
+app.listen(process.env.PORT || 5555, () => console.log('App is listening...'))
 
 
 function calculateRate(weight, type) {
+    var ic; // initial cost
+    var r;  // rate
+    var inc = 1; // rate of incrementing cost, assumes 1 by default 
+    var max;
+    // I had to copy/paste all these values in. I couldn't find a quicker way to do this
     if (type == "stamped") {
-        return weight * .5;
+        ic = 0.50;
+        r = .21;
+        max = 3.5;
     }
     else if (type == "metered") {
-        return weight * 1.5;
+        ic = 0.47;
+        r = .21;
+        max = 3.5;
     }
     else if (type == "flats") {
-        return weight * 2.5;
+        ic = 1.00;
+        r = .21;
+        max = 13;
+    }
+    else if (type == "first-class") {
+        ic = 3.50;
+        r = .25;
+        max = 13;
     }
     else {
-        return weight * 7;
+        return "Incorrect type specified. Please return to the form.";
     }
+    if (weight <= 0 || weight > max) {
+        return "Weight is incorrect. It must weigh more than 0 and less than or equal to " + max + " ounces for type \"" + type + "\".";
+    }
+    
+    var cost;
+    // Have to treat first-class in a special way since it formats differently than the rest
+    if (type == "first-class") {
+        cost = ic;
+        var i;
+        for (i = 4; i < 8 && i < weight; i += 4) {
+            console.log(cost);
+            cost += r;
+            console.log(cost)
+        }
+        r = 0.35;
+        for (i; i < weight && i >= 8; i++) {
+            cost += r;
+        }
+        
+    }
+    else {
+        cost = ic;
+        for (var i = 1; i < weight; i++) {
+            cost += r;
+        }
+    }
+
+    // always round to and display 2 decimal places.
+    return (Math.round(cost * 100) / 100).toFixed(2);
+
+
 }
