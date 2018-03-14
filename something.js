0adeadbe762fb30c44e730cb5a4d07008c37223c
keanedawg <cameronfife@hotmail.com>
2018-03-13 20:17:59 -0600

    added port number

--- something.js
+++ something.js
@@ -29,7 +29,7 @@ app.get('/calcrate/:weight/:type', function(req, res){
     res.render('getRate.ejs',{rate:calculateRate(req.params.weight, req.params.type)});
 });
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'))
 
 
 function calculateRate(weight, type) {
